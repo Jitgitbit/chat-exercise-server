2b5fc58f3fd5e44d03e6e1fcc0f9941f5eeb8724
Jitgitbit <rowowolf@gmail.com>
2020-02-14 17:08:13 +0100

    FACTORIES !!!!

--- message/router.js
+++ message/router.js
@@ -1,64 +1,65 @@
 const express = require('express')
 const Message = require('./model')
-const Sse = require('json-sse')
 
-const { Router } = express
+function factory (stream) {
+  const { Router } = express
 
-const stream = new Sse()
+  const router = Router()
 
-const router = Router()
+  // get on the stream
+  router.get(
+    '/stream',
+    async (request, response, next) => {
+      try {
+        const messages = await Message
+          .findAll()
 
-// get on the stream
-router.get(
-  '/stream',
-  async (request, response, next) => {
-    try {
-      const messages = await Message
-        .findAll()
+        const action = {
+          type: 'ALL_MESSAGES',
+          payload: messages
+        }
 
-      const action = {
-        type: 'ALL_MESSAGES',
-        payload: messages
+        const json = JSON
+          .stringify(action)
+
+        stream.updateInit(json)
+        stream.init(request, response)
+      } catch (error) {
+        next(error)
       }
-
-      const json = JSON
-        .stringify(action)
-
-      stream.updateInit(json)
-      stream.init(request, response)
-    } catch (error) {
-      next(error)
     }
-  }
-)
+  )
 
-router.post(
-  '/message',
-  async function (
-    request, response, next
-  ) {
-    try {
-      const { body } = request
-      const { text } = body
-      const entity = { text }
-      const message = await Message
-        .create(entity)
+  router.post(
+    '/message',
+    async function (
+      request, response, next
+    ) {
+      try {
+        const { body } = request
+        const { text } = body
+        const entity = { text }
+        const message = await Message
+          .create(entity)
 
-      const action = {
-        type: 'ONE_MESSAGE',
-        payload: message
+        const action = {
+          type: 'ONE_MESSAGE',
+          payload: message
+        }
+
+        const json = JSON
+          .stringify(action)
+
+        stream.send(json)
+
+        response.send(message)
+      } catch (error) {
+        next(error)
       }
-
-      const json = JSON
-        .stringify(action)
-
-      stream.send(json)
-
-      response.send(message)
-    } catch (error) {
-      next(error)
     }
-  }
-)
+  )
 
-module.exports = router
+  return router
+}
+
+module.exports = factory
