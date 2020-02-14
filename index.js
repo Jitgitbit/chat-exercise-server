3b4d207f0130ef20107000968fcedb5312e42e4b
Jitgitbit <rowowolf@gmail.com>
2020-02-14 13:45:43 +0100

    cors

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const messageRouter = require('./message/router')
+const cors = require('cors')
 
 const app = express()
 
@@ -8,10 +9,12 @@ const port = process.env.PORT || 4000
 function onListen () {
   console.log(`listening on : ${port}`)
 }
+const corsMiddleware = cors()
 
 const jsonMiddleware = express
   .json()
 
+app.use(corsMiddleware);
 app.use(jsonMiddleware);
 app.use(messageRouter);
 
