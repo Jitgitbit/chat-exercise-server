eeeec9276f970e4a58cb0b473fdc2648fafedc1e
Jitgitbit <rowowolf@gmail.com>
2020-02-15 19:13:38 +0100

    force once more

--- db.js
+++ db.js
@@ -5,7 +5,7 @@ const databaseUrl = process.env.DATABASE_URL || 'postgres://postgres:secret@loca
 const db = new Sequelize(databaseUrl)
 
 db
-  .sync({ force: true })// forces by overwriting,  the latest change counts!
+  .sync({ force: true })// forces by overwriting,  the latest change counts!!
   // .sync()
   .then(() => console.log('Database connected'))
   .catch(console.error)
